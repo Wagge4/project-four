8c0d551c4856fe97c859f8dfda1f2df6f7bae83f
Wagge4 <markus.wallgren4@gmail.com>
2022-09-21 13:01:51 +0000

    Add variables to JS file

--- todo/static/javascript/script.js
+++ todo/static/javascript/script.js
@@ -1,1 +1,5 @@
-console.log('Hello, World!')
+const container = document.querySelector(".container");
+const count = document.getElementById("count");
+const movieSelect = document.getElementById("movie");
+const total = document.getElementById("total");
+const seats = document.querySelectorAll(".row .seat:not(.booked)");
