4c7fe4a03432be902439e3150574d6124a5ccdfc
Wagge4 <markus.wallgren4@gmail.com>
2022-09-21 16:03:09 +0000

    Add function to update the total and the count

--- todo/static/javascript/script.js
+++ todo/static/javascript/script.js
@@ -12,3 +12,18 @@ function setMovieData(movieIndex, moviePrice) {
     localStorage.setItem("selectedMovieIndex", movieIndex);
     localStorage.setItem("selectedMoviePrice", moviePrice);
 }
+
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  
+    localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+  
+    const selectedSeatsCount = selectedSeats.length;
+  
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+  
+    setMovieData(movieSelect.selectedIndex, movieSelect.value);
+}
