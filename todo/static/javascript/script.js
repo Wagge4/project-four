bdda6d4d2079a14ec3f8d7e588fe2f9c144cda12
Wagge4 <markus.wallgren4@gmail.com>
2022-09-21 15:51:41 +0000

    Add function to save price and movie index

--- todo/static/javascript/script.js
+++ todo/static/javascript/script.js
@@ -3,3 +3,12 @@ const count = document.getElementById("count");
 const movieSelect = document.getElementById("movie");
 const total = document.getElementById("total");
 const seats = document.querySelectorAll(".row .seat:not(.booked)");
+
+populateUI();
+
+let ticketPrice = +movieSelect.value;
+
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem("selectedMovieIndex", movieIndex);
+    localStorage.setItem("selectedMoviePrice", moviePrice);
+}
