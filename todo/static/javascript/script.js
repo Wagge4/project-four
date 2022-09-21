58aa5b5ec3403afce7383f820b6aa6a74742881f
Wagge4 <markus.wallgren4@gmail.com>
2022-09-21 16:07:45 +0000

    Add function to get data fom local storage

--- todo/static/javascript/script.js
+++ todo/static/javascript/script.js
@@ -27,3 +27,23 @@ function updateSelectedCount() {
   
     setMovieData(movieSelect.selectedIndex, movieSelect.value);
 }
+
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+      seats.forEach((seat, index) => {
+        if (selectedSeats.indexOf(index) > -1) {
+          console.log(seat.classList.add("selected"));
+        }
+      });
+    }
+  
+    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  
+    if (selectedMovieIndex !== null) {
+      movieSelect.selectedIndex = selectedMovieIndex;
+      console.log(selectedMovieIndex);
+    }
+  }
+  console.log(populateUI());
